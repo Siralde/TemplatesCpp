26a6f5a0f28bd90db206b8e652353b80e01b8a92
SirAG <aldemarojgs@gmail.com>
2018-09-07 02:25:10 +0200

    C++ Functions Templates

---
+++ Functions.cpp
@@ -0,0 +1,27 @@
+/**
+ * @file b1_1_laboratorio.cpp
+ *
+ * @brief Ejercicio 01: Boletin 1.
+ *
+ * Descripcion
+ *
+ * @version 1
+ * @author Aldemaro Gonzalez
+ * @date //2017
+ */
+
+//Declarar Funcion
+
+/************************ MostrarVector *********************//
+*
+* Muestra el vector por pantalla
+*
+*
+* @param[in] v Vector
+* @param[in] tam Numero de Elementos
+*
+*//*************************************************************/
+int Div(int x, int y, int & resto)
+{
+    ...
+}
