f96d4b32176db691e297954bbdf86c12fe9c5cee
SirAG <aldemarojgs@gmail.com>
2018-09-07 02:27:19 +0200

    Dumb C++ Template

---
+++ Main.cpp
@@ -0,0 +1,42 @@
+/**
+ * @file b1_1_laboratorio.cpp
+ *
+ * @brief Ejercicio 01: Boletin 1.
+ *
+ * Descripcion
+ *
+ * @version 1
+ * @author Aldemaro Gonzalez
+ * @date //2017
+ */
+
+#include <iostream>
+using namespace std;
+
+const float NOMBRE = ; ///<Cambio de pulgadas a metros
+
+/**
+ * Funcion principal
+ */
+int main (void)
+{
+    float
+    
+    //Empezamos el programa explicando al usuario lo que hace
+    cout << ".\n\n";
+    
+    
+    //Pedimos al usurario
+    cout << " ";
+    cin >> metros;
+    
+    /*
+     * Procesamos la informacion
+     */
+    
+    // Mostramos los resultados obtenidos
+    cout << ;
+    
+    return 0;
+}
+
