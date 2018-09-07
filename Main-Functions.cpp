d4501fcc40b7393a31a04d43afffe1c5ca3878fa
SirAG <aldemarojgs@gmail.com>
2018-09-07 02:26:42 +0200

    C++ Main and Functions Template

---
+++ Main-Functions.cpp
@@ -0,0 +1,56 @@
+/**
+ * @file b1_1_laboratorio.cpp
+ *
+ * @brief Ejercicio 01: Boletin 1.
+ *
+ * Descripcion
+ *
+ * @version 1
+ * @author Aldemaro Gonzalez
+ * @date //2017
+ */
+
+#include <iostream>
+using namespace std;
+
+//Declarar Funcion
+const float NOMBRE = ; ///<Cambio de pulgadas a metros
+
+/**
+ * Funcion principal
+ */
+int main (void)
+{
+    float
+    
+    //Empezamos el programa explicando al usuario lo que hace
+    cout << ".\n\n";
+    
+    
+    //Pedimos al usurario
+    cout << " ";
+    cin >> metros;
+    
+    /*
+     * Procesamos la informacion
+     */
+    
+    // Mostramos los resultados obtenidos
+    cout << ;
+    
+    return 0;
+}
+
+/************************ MostrarVector *********************//
+*
+* Muestra el vector por pantalla
+*
+*
+* @param[in] v Vector
+* @param[in] tam Numero de Elementos
+*
+*//*************************************************************/
+int Div(int x, int y, int & resto)
+{
+    ...
+}
